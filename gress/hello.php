4119735af7fc379186779500cecc9756f614461a
Jodi Jonatan <jdi102350@gmail.com>
2026-04-17 09:09:47 +0700

    have learned about if statement

---
+++ gress/hello.php
@@ -0,0 +1,29 @@
+<?php
+
+$nama = "gres";
+$umur = 15;
+$kelas = "X rpl 2";
+$hobi = "nyanyi";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <h1>
+    <?php
+    echo "hello nama saya,$nama,umur saya $umur, kelas saya $kelas,hobi saya $hobi";
+
+    ?>
+  </h1>
+</body>
+
+</html>
